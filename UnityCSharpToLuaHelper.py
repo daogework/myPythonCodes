4579c6984978c9e2d85644f888d8dac367e0684d
daoge <daogework@gmail.com>
2020-09-09 15:42:22 +0800

    基本功能完成，目前无法判断成员变量，需要手动修改

--- UnityCSharpToLuaHelper.py
+++ UnityCSharpToLuaHelper.py
@@ -22,17 +22,23 @@ _var_1= compile("{space}var {v1} = {v2};")
 _v_1= compile("        {v1} = {v2};")
 __findlist= compile("{}new List<{Type}>();")
 _private_member= compile("{space}private {type} {member_name} = {value};")
+_private_member_empty= compile("{space}private {type} {member_name};")
 _public_member= compile("{space}public {type} {member_name} = {value};")
 _public_member_empty= compile("{space}public {type} {member_name};")
 _get_set = compile("{space}public {type} {value_name} {")
 _lambda_param = compile("{}(({param}) => {")
-
 _msgcenter_event = compile("{space}MessageCenter.Instance.{}({}, {callbackfunc});")
 _ReferenceEquals = compile("{}ReferenceEquals({value}, null){}")
+_Instance_call = compile("{}Instance.{funcname}({parm});")
+_for_i = compile("{}for (int i = 0; i < {param}; i++)")
+_for_i_r = compile("{}for (int i = {param}; i >= 0; i--){}")
+_fangkuohao = compile("{}<{typename}>(){}")
+_add_sum = compile("{space} {v1} += {v2};")
 
 
-#fout = open('H:\\python脚本\\__fortest/out.lua', 'w')
-#sys.stdout = fout
+
+fout = open('D:\\python脚本\\__fortest/out.lua', 'w')
+sys.stdout = fout
 
 total = 0
 scoreList = []
@@ -136,18 +142,39 @@ def process(index, line, lines):
         lambdastr = lambdastr[:k] + "end" + lambdastr[k+1:]
         return lambdastr
     
+    index_as = line.find(' as ')
+    if index_as!=-1:
+        index_f = line.find(';', index_as)
+        s = line[index_as:index_f]
+        line = line.replace(s, ';')
+
+    # r = 
+    # if not r is None:
+    #     typename = r['typename']
+    #     s = '<'+typename+'>'
+    #     rs = '--TODO'
+    #     line = line.replace(s, rs)
+
     r = _var_1.parse(line)
     if not r is None:
         space = r['space']
         v1 = r['v1']
         v2 = r['v2']
-        s = space+'local '+v1+' = '+v2+'\n'
+        s = space+'local '+v1+' = '+v2+';\n'
         r = __findlist.parse(s)
         if not r is None:
             listtype = r['Type']
             liststr = 'new List<'+listtype+'>()'
-            s=s.replace(liststr,'{}')
-        return s
+            s=s.replace(liststr,'{};')
+        r = _fangkuohao.parse(s)
+        todo = ''
+        if not r is None:
+            typename = r['typename']
+            ss = '<'+typename+'>'
+            s=s.replace(ss,'')
+            todo = ' --TO DO 上面一句是方括号类型 typename='+typename+'\n'
+           
+        return s+todo
 
     r = _v_1.parse(line)
     if not r is None:
@@ -159,16 +186,23 @@ def process(index, line, lines):
                 break
         v1 = r['v1']
         v2 = r['v2']
-        s = 'self.'+v1+'='+v2+'\n'
+        s = 'self.'+v1+'='+v2+';\n'
         s = s.replace(' ','')
         s = s.replace('=',' = ')
         r = __findlist.parse(line)
         if not r is None:
             listtype = r['Type']
             liststr = 'newList<'+listtype+'>()'
-            s=s.replace(liststr,'{}')
+            s=s.replace(liststr,'{};')
+        r = _fangkuohao.parse(s)
+        todo = ''
+        if not r is None:
+            typename = r['typename']
+            ss = '<'+typename+'>'
+            s=s.replace(ss,'')
+            todo = ' --TO DO 上面一句是方括号类型 typename='+typename+'\n'
         
-        return space+s
+        return space+s+todo
     
     r = _private_member.parse(line)
     if not r is None:
@@ -176,16 +210,23 @@ def process(index, line, lines):
         member_name=r['member_name']
         value=r['value']
         space=r['space']
-        return space+member_name+' = '+value+'\n'
+        return space+member_name+' = '+value+',\n'
     r = _public_member.parse(line)
     if not r is None:
         member_name=r['member_name']
         value=r['value']
         space=r['space']
-        return space+member_name+' = '+value+'\n'
+        return space+member_name+' = '+value+',\n'
     r = _public_member_empty.parse(line)
     if not r is None:
-        return ''
+        member_name=r['member_name']
+        space=r['space']
+        return space+member_name+' = nil,\n'
+    r = _private_member_empty.parse(line)
+    if not r is None:
+        member_name=r['member_name']
+        space=r['space']
+        return space+member_name+' = nil,\n'
     r = _get_set.parse(line)
     if not r is None:
         #{space}public {type} {value_name} {
@@ -194,7 +235,7 @@ def process(index, line, lines):
         passCount = count_get_set_line(index, lines)
         getstr = find_get(index+1, lines)
         setstr = find_set(index+1, lines, value_name)
-        return space+value_name+' = {\n' + getstr + setstr+space+'}\n'
+        return space+value_name+' = {\n' + getstr + setstr+space+'},\n'
     
     r = _ReferenceEquals.parse(line)
     if not r is None:
@@ -204,10 +245,54 @@ def process(index, line, lines):
         line = line.replace(originalstr, newstr)
         return line
 
+    #_Instance_call = compile("{}Instance.{funcname}({parm});")
+    r = _Instance_call.parse(line)
+    if not r is None:
+        funcname = r['funcname']
+        parm = r['parm']
+        s = 'Instance.'+funcname+'('+parm+')'
+        rs = 'Instance:'+funcname+'('+parm+')'
+        return line.replace(s, rs)
+
+    #_for_i = compile("{}for (int i = 0; i < {param}; i++)")
+    #_for_i_r = compile("{}for (int i = {param}; i >= 0; i--)")
+    r = _for_i.parse(line)
+    if not r is None:
+        param = r['param']
+        s = 'for (int i = 0; i < '+param+'; i++)'
+        rs = 'for  i = 0, ('+param+'-1) do'
+        return line.replace(s, rs)
+
+    r = _for_i_r.parse(line)
+    if not r is None:
+        param = r['param']
+        s = 'for (int i = '+param+'; i >= 0; i--)'
+        rs = 'for  i = ('+param+'), 0, -1 do'
+        return line.replace(s, rs)
+
+    #_add_sum = compile("{}{v1} += {v2};")
+    r = _add_sum.parse(line)
+    if not r is None:
+        v1 = r['v1']
+        v2 = r['v2']
+        v1 = v1.replace(' ','')
+        s = v1 + ' += ' + v2
+        rs = v1 +' = '+ v1+' + '+v2
+        return line.replace(s, rs)
+
     if line.startswith('    }'):
         return '    end,\n'
+    if 'do {' in line:
+        for i in range(index, len(lines)):
+            l = lines[i]
+            if 'while (' in l:
+                line ='--TODO 这里是do while,需要处理\n'+ l.replace('\n','') +' do {\n'
+                lines[i] = ''
+        return line
     return line
 
+
+##########################################
 def count_get_set_line(index, lines):
     counter = 0
     lineCounter = 0
@@ -245,8 +330,7 @@ def find_get(index, lines):
            # getstr = getstr.replace('\n',' ')
             getstr = getstr.replace('get','get = function(self)')
             getstr = getstr.replace('{','')
-            getstr = getstr.replace('}','end')
-            getstr = getstr.replace(';','')
+            getstr = getstr.replace('}','end,')
             getstr = getstr.replace('return ','return self.')
             return getstr
     return ''
@@ -259,10 +343,9 @@ def find_set(index, lines, setfieldname):
         if 'set' in line:
             setstr = find_end_of_parantheses(lineindex, lines)
             #setstr = setstr.replace('\n','**')
-            setstr = setstr.replace('set','set = function(self)')
+            setstr = setstr.replace('set','set = function(self, value)')
             setstr = setstr.replace('{','')
-            setstr = setstr.replace('}','end')
-            setstr = setstr.replace(';','')
+            setstr = setstr.replace('}','end,')
             #setstr = setstr.replace('**','\n')  
             setstr = setstr.replace(setfieldname,'self.'+setfieldname)
             return setstr
@@ -289,7 +372,7 @@ def paserFunc(line, paser, paserparam):
 def replaceByIndex(s, index, newstring):
      return s[:index] + newstring + s[index + 1:]
 
-def fine_end_of_start_end(index, rstr, start, end):#找到括号尾部index
+def find_end_of_start_end(index, rstr, start, end):#找到括号尾部index
     count = 0
     isFoundFirst = False
     for i in range(index,len(rstr)):
@@ -303,7 +386,7 @@ def fine_end_of_start_end(index, rstr, start, end):#找到括号尾部index
             return i
     return -1
 
-def fine_end_of_start_end2(index, rstr, start, startlen, end):#找到括号尾部index
+def find_end_of_start_end2(index, rstr, start, startlen, end):#找到括号尾部index
     count = 0
     isFoundFirst = False
     for i in range(index,len(rstr)):
@@ -329,45 +412,90 @@ def iscontinueWith(startindex,rstr, char):
             break
     return False
 
-def handleIfElse(rstr):
+def handleIfElseDo(rstr):
     willreplcacetoendlist = []
-    index = rstr.find('if')
+    index = rstr.find('if ')
     while index != -1:
-        index+=2
-        
-        bindex = fine_end_of_start_end(index, rstr,'(',')')
+        index+=3
+        bindex = find_end_of_start_end(index, rstr,'(',')')
         rstr = replaceByIndex(rstr, bindex, ') then')
-        index = rstr.find('if', bindex)
+        index = rstr.find('if ', bindex)
         
     index = rstr.find('else')
-    while index != -1:
+    temp = rstr[index+4]
+    while index != -1 and (temp =='\n' or temp ==' '):
         if iscontinueWith(index+4, rstr, '{'):
-            bindex = fine_end_of_start_end(index, rstr,'{','}')
+            bindex = find_end_of_start_end(index, rstr,'{','}')
             assert(bindex!=-1)
             willreplcacetoendlist.append(bindex)
             index = rstr.find('else', index+1)
         else:
+            else_enter = rstr.find('\n', index-1)
+            main_enter = rstr.find('\n', else_enter+1)
+            temp = rstr[else_enter+1:main_enter]
+            if (not '\n' in temp)and(';'in temp):
+                rstr = replaceByIndex(rstr, main_enter-1, 'β')
             index = rstr.find('else', index+1)
 
+    index = rstr.find('else {')
+    while index != -1:
+        bindex = find_end_of_start_end(index-1, rstr,'{','}')
+        assert(bindex!=-1)
+        willreplcacetoendlist.append(bindex)
+        index = rstr.find('else {', index+1)
+
     index = rstr.find('then')
-    while index != -1:#思路 找到then后面的括号匹配,难点：有点then后面没有括号，要排除
+    while index != -1:
         if iscontinueWith(index+4, rstr, '{'):
-            bindex = fine_end_of_start_end(index, rstr,'{','}')
+            bindex = find_end_of_start_end(index, rstr,'{','}')
             assert(bindex!=-1)
             if rstr[bindex+1]=='\n':
                 willreplcacetoendlist.append(bindex)
             index = rstr.find('then', index+1)
         else:
+            then_enter = rstr.find('\n', index)
+            main_enter = rstr.find('\n', then_enter+1)
+            after_main_enter = rstr.find('\n', main_enter+1)
+            temp = rstr[main_enter:after_main_enter+1]
+            #test = rstr[then_enter:after_main_enter]
+            if not('else\n' in temp or 'else ' in temp):
+                if rstr[main_enter-1]==';':
+                   rstr = replaceByIndex(rstr, main_enter-1, 'β')
+               # rstr = replaceByIndex(rstr, main_enter, 'β')  
             index = rstr.find('then', index+1)
 
+    
+    index = rstr.find('do ')
+    while index != -1:
+        if iscontinueWith(index+3, rstr, '{'):
+            bindex = find_end_of_start_end(index, rstr,'{','}')
+            assert(bindex!=-1)
+            if rstr[bindex+1]=='\n':
+               # test = rstr[index:bindex]
+               # print(test)
+               # print(rstr[bindex])
+                willreplcacetoendlist.append(bindex)
+        index = rstr.find('do', index+1)
+
+    index = rstr.find('do\n')
+    while index != -1:
+        else_enter = rstr.find('\n', index-1)
+        main_enter = rstr.find('\n', else_enter+1)
+        temp = rstr[else_enter+1:main_enter]
+        #test = rstr[else_enter:main_enter]
+        if (not '\n' in temp)and(';'in temp):
+            rstr = replaceByIndex(rstr, main_enter-1, 'β')
+        index = rstr.find('do\n', index+1)
+
     for index in willreplcacetoendlist:
-        rstr = replaceByIndex(rstr, index, '#')
+        rstr = replaceByIndex(rstr, index, 'α')
 
     rstr = rstr.replace('else {','else')
     rstr = rstr.replace(' then {',' then')
+    rstr = rstr.replace(' do {',' do')
     rstr = rstr.replace('} else','else')
-    rstr = rstr.replace('#','end ')
-
+    rstr = rstr.replace('α','end')
+    rstr = rstr.replace('β',' end')
     return rstr
 
 def main():
@@ -375,23 +503,27 @@ def main():
     for index, line in enumerate(lines):
         resultStr += process(index,line, lines)
     
-    resultStr = resultStr.replace(';','')
+    
     resultStr = resultStr.replace('//','--')
     resultStr = resultStr.replace('.ToString()',':ToString()')
     resultStr = resultStr.replace('new ','')
     resultStr = resultStr.replace(' && ',' and ')
     resultStr = resultStr.replace(' != ',' ~= ')
     resultStr = resultStr.replace('!(','not(')
+    resultStr = resultStr.replace('(!','(not ')
     
     resultStr = resultStr.replace('else if','elseif')
 
-    resultStr = handleIfElse(resultStr)
-
+    resultStr = handleIfElseDo(resultStr)
+    resultStr = resultStr.replace(';','')
     #resultStr = resultStr.replace(' then {',' then')
     
     
-
-
+    resultStr = resultStr.replace('void ','')
+    resultStr = resultStr.replace('= null','= nil')
+    resultStr = resultStr.replace('List.Clear()','List:Clear()')
+    # resultStr = resultStr.replace('--TODO() end','() end --TODO')
+    # resultStr = resultStr.replace('newList() ','{} ')
 
 main()
 
---
+++ __fortest/out.lua
@@ -0,0 +1,202 @@
+
+local CommonModule = {
+    ctor = function(self)
+        self.AutoRegister = true
+        self.itemList = {}
+        self.levelUpItemList = {}
+    end,
+
+    waitLockCount = 0,
+    WaitLockCount = {
+        get = function(self)  return self.waitLockCount end,
+        set = function(self, value) 
+            if (value > 0) then
+                UIManager.Instance.OpenUI(EnumUIType.WaitResponseUI)
+            else
+                UIManager.Instance.CloseUI(EnumUIType.WaitResponseUI) end
+            self.waitLockCount = value
+        end,
+    },
+
+    OnLoad = function(self)
+        local oldonPlayerLevelChanged = self.onPlayerLevelChanged
+        self.onPlayerLevelChanged = function(msg) oldonPlayerLevelChanged(self, msg) end
+        MessageCenter.Instance:AddListener(MsgType.CLIENT_PLAYER_LEVEL, self.onPlayerLevelChanged)
+
+        local oldonPlayerVIPLevelChanged = self.onPlayerVIPLevelChanged
+        self.onPlayerVIPLevelChanged = function(msg) oldonPlayerVIPLevelChanged(self, msg) end
+        MessageCenter.Instance:AddListener(MsgType.CLIENT_PLAYER_VIPLEVEL, self.onPlayerVIPLevelChanged)
+
+        local oldmessageBroadcastNtf = self.messageBroadcastNtf
+        self.messageBroadcastNtf = function(msg) oldmessageBroadcastNtf(self, msg) end
+        MessageCenter.Instance:AddListener(MsgType.NET_MESSAGEBROADCAST_NTF, self.messageBroadcastNtf)
+
+    end,
+
+    OnRelease = function(self)
+        MessageCenter.Instance:RemoveListener(MsgType.CLIENT_PLAYER_LEVEL, self.onPlayerLevelChanged)
+        MessageCenter.Instance:RemoveListener(MsgType.CLIENT_PLAYER_VIPLEVEL, self.onPlayerVIPLevelChanged)
+        MessageCenter.Instance:RemoveListener(MsgType.NET_MESSAGEBROADCAST_NTF, self.messageBroadcastNtf)
+    end,
+
+    Update = function(self)
+        --  Debug.Log("CommonModule Update")
+        if (not openUI) then
+            if (not(circleBroadcastList==nil) and circleBroadcastList.Count > 0) then
+                local serverTime = TimeHelper.GetServerTimestamp()
+                if (circleBroadcastList.First().Timestamp <= serverTime) then
+                    UIManager.Instance:OpenUI(EnumUIType.SystemNoticeUI)
+                    self.openUI = true
+                end
+            end
+        end
+    end,
+
+    --#region award
+    itemList = nil,
+    SetItemData = function(self,itemInfos,action)
+        itemList:Clear()
+        if (not(itemInfos==nil)) then
+            local len = itemInfos.Length
+            for  i = 0, (len-1) do
+                itemList.Add(itemInfos[i]) end
+        end
+        UIManager.Instance:OpenUI(EnumUIType.AwardUI, action)
+    end,
+    SetPlayerCoin = function(self)
+        --local player = ((GamePlayer)GlobalLuaShareWithCSharp.GetObj("GamePlayer"))
+    end,
+    GetShowItemList = function(self)
+        return itemList
+    end,
+    GetShowLevelUpItemList = function(self)
+        return levelUpItemList
+    end,
+    --#endregion
+
+    --#region levelUp
+    levelUpItemList = nil,
+    onPlayerLevelChanged = function(self,msg)
+        levelUpItemList:Clear()
+        local itemArr = msg["Rewards"]
+        if (not(itemArr==nil)) then
+            local len = itemArr.Length
+            for  i = 0, (len-1) do
+                levelUpItemList.Add(ItemInfo(itemArr[i].item_id, itemArr[i].item_sub_id, itemArr[i].item_count)) end
+        end
+        UIManager.Instance:OpenUI(EnumUIType.LevelUpUI)
+    end,
+
+    onPlayerVIPLevelChanged = function(self,msg)
+        local isChanged = bool.Parse(msg["VIPLevelChange"]:ToString())
+        if (isChanged) then
+            UIManager.Instance:OpenUI(EnumUIType.VIPLevelUpUI)
+        end
+    end,
+    --#endregion
+
+    --#region 系统消息
+    --消息通知
+    messageBroadcasQueue = nil,
+    circleBroadcastList = nil,
+    openUI = false,
+    messageBroadcastNtf = function(self,msg)
+        local rsp = msg.Content
+        if ((messageBroadcasQueue==nil)) then
+            self.messageBroadcasQueue = newQueue() end
+ --TO DO 上面一句是方括号类型 typename=CLPFMessageBroadcastNtf
+        if (rsp.type == 12) then
+            if ((circleBroadcastList==nil)) then
+                self.circleBroadcastList = {} end
+
+            parseCircleBroadcast(rsp)
+
+            if (not openUI) then
+                UIManager.Instance:OpenUI(EnumUIType.SystemNoticeUI)
+                self.openUI = true
+            end
+        elseif (rsp.type == 13) then
+            local contents = JArray.Parse(rsp.content)
+            local msgId = int.Parse(contents[0]:ToString())
+            local count = circleBroadcastList.Count
+            for  i = (count - 1), 0, -1 do
+                local broadcast = circleBroadcastList[i]
+                if (msgId == broadcast.Id) then
+                    circleBroadcastList.Remove(broadcast) end
+            end
+        else
+            messageBroadcasQueue.Enqueue(rsp)
+            if (not openUI) then
+                UIManager.Instance:OpenUI(EnumUIType.SystemNoticeUI)
+                self.openUI = true
+            end
+        end
+    end,
+
+    GetMessageBroadcastDequeue = function(self)
+        self.CLPFMessageBroadcastNtfntf = nil
+
+        if (not(circleBroadcastList==nil) and circleBroadcastList.Count > 0) then
+            local serverTime = TimeHelper.GetServerTimestamp()
+            if (circleBroadcastList.First().Timestamp <= serverTime) then
+                self.ntf = circleBroadcastList.First().Ntf
+                circleBroadcastList.RemoveAt(0)
+                return ntf
+            end
+        end
+
+        if (not(messageBroadcasQueue==nil) and messageBroadcasQueue.Count > 0) then
+            self.ntf = messageBroadcasQueue.Dequeue()
+        else
+            self.messageBroadcasQueue = nil
+            self.openUI = false
+        end
+        return ntf
+    end,
+
+    OnCloseSystemNotice = function(self)
+        self.openUI = false
+    end,
+
+    parseCircleBroadcast = function(self,rsp)
+        local contents = JArray.Parse(rsp.content)
+        local msgId = int.Parse(contents[0]:ToString())
+        local sendTime = ulong.Parse(contents[1]:ToString()) * 1000
+        local lastTime = ulong.Parse(contents[2]:ToString()) * 1000
+        local interval = ulong.Parse(contents[3]:ToString()) * 1000
+        local message = contents[4]:ToString()
+        local endTime = sendTime + lastTime
+        local curTime = sendTime
+
+--TODO 这里是do while,需要处理
+        while (curTime <= endTime) do
+            circleBroadcastList.Add(CircleBroadcastMessage(msgId, curTime, message, rsp))
+            curTime = curTime + interval
+        end
+
+        sortCircleBroadcast()
+    end,
+
+    sortCircleBroadcast = function(self)
+        circleBroadcastList.Sort(function(a, b)
+            if (a.Timestamp > b.Timestamp) then return 1
+            elseif (a.Timestamp == b.Timestamp) then return 0
+            else return -1 end
+        end)
+    end,
+    --#endregion
+}
+
+local CircleBroadcastMessage = {
+    Id = nil,
+    Timestamp = nil,
+    Message = nil,
+    Ntf = nil,
+
+    ctor = function(self,id,timestamp,message,ntf)
+        self.Id = id
+        self.Timestamp = timestamp
+        self.Message = message
+        self.Ntf = ntf
+    end,
+}
